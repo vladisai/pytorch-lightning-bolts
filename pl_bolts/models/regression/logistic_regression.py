045c2d24c01c426a3ef92a508cdd1ea797d8d731
Jirka Borovec <jirka.borovec@seznam.cz>
2020-08-05 01:02:44 +0200

    cli: reg

--- pl_bolts/models/regression/logistic_regression.py
+++ pl_bolts/models/regression/logistic_regression.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -127,7 +129,6 @@ class LogisticRegression(pl.LightningModule):
 
 
 def run_cli():
-    from argparse import ArgumentParser
     pl.seed_everything(1234)
 
     # Example: Iris dataset in Sklearn (4 features, 3 class labels)
